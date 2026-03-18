afe9602bd66a89d52c734244bc75b93a6337c80e
adhilunnikrishnan <adhilunnikrishnanofficial@gmail.com>
2026-03-18 23:04:31 +0530

    feat: implement user authentication with registration and login functionality

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,7 @@
 import connectToDatabase from "../config/db.js";
 import argon2 from "argon2";
-import { v7 as uuidv7 } from 'uuid';
+import { v7 as uuidv7 } from "uuid";
+import jwt from "jsonwebtoken";
 
 export const registerUser = async (req, res) => {
   console.log("register user trigerd>>>>>>");
@@ -9,41 +10,87 @@ export const registerUser = async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
-    if(!username || !email || !password) {
-      return res.status(400).json({ message: "Username, email and password are required" });
+    if (!username || !email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Username, email and password are required" });
     }
     const db = await connectToDatabase();
 
     const existingUser = await db.collection("users").findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ message: "User with this email already exists" });
+      return res
+        .status(400)
+        .json({ message: "User with this email already exists" });
     }
     const hashedPassword = await argon2.hash(password);
-    console.log("hashed password>>>>>",hashedPassword)
+    console.log("hashed password>>>>>", hashedPassword);
 
     const inputData = {
       username,
       email,
-      password:hashedPassword,
-      userId:uuidv7(),
+      password: hashedPassword,
+      userId: uuidv7(),
       createdAt: new Date(),
       isBlocked: false,
       isDeleted: false,
-    }
-    console.log("stuctured data",inputData)
+    };
+    console.log("stuctured data", inputData);
 
     const result = await db.collection("users").insertOne(inputData);
-    console.log("database query result",result)
+    console.log("database query result", result);
 
-    res.status(200).json({ message: "User registered successfully",result });
-
+    res.status(200).json({ message: "User registered successfully", result });
   } catch (error) {
     console.error("Error registering user:", error);
     res.status(500).json({ message: "Internal server error" });
   }
-
 };
 
 export const loginUser = async (req, res) => {
-  // Implementation for user login
+  console.log("login user function triggered");
+  console.log("login user data", req.body);
+
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "email and password required" });
+    }
+    const db = await connectToDatabase();
+
+    const existingUser = await db.collection("users").findOne({ email });
+
+    console.log("data of existing user>>>>>>", existingUser);
+    //
+    if (!existingUser) {
+      return res
+        .status(400)
+        .json({ message: "User with this email not exists" });
+    }
+
+    const passwordVerification = await argon2.verify(
+      existingUser.password,
+      password,
+    );
+    if (!passwordVerification) {
+      return res.status(400).json({ message: "email or password incorrect" });
+    }
+
+    const token = jwt.sign(
+      {
+        username: existingUser.username,
+        userId: existingUser.userId,
+        email: existingUser.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d", // expires in 1 day
+      },
+    );
+    console.log("jwt token>>>>>>>>>>>", token);
+
+    res
+      .status(200)
+      .json({ message: "User Login successfully", jwtToken: token });
+  } catch (error) {}
 };
